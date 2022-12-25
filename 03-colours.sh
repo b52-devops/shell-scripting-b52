2ec18a2e78776ed43eaf74ee52f7d099b21dde1f
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-24 22:59:31 -0500

    add lines in  echo script

--- 03-colours.sh
+++ 03-colours.sh
@@ -9,3 +9,8 @@ echo -e "\e[31m I am printing Red Color \e[0m"
 echo -e "\e[33m I am printing Yellow Color \e[0m"
 echo -e "\e[34m I am printing Blue Color \e[0m"
 echo -e "\e[35m I am printing Cyan Color \e[0m"
+
+# How to print background color ?
+
+echo -e "\e[41;33m Welcome to colors \e[0m "
+echo -e "\e[47;32m Avatar2 is a very good filn \e[0m "
