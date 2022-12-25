b57b4cbe9caf30999392a03698fbc9b632141431
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-24 22:45:48 -0500

    add lines in  echo script

---
+++ 02-sample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Escape sequesnce characters
+
+echo "Hello World"
+
+# Escape sequesnce characters will be considered by echo command only if we enable it by using the -e option
+
+# \n : new line
+# \t : tab space
+
+echo -e "Line1\nLine2"
+echo Line2
+echo Line3
