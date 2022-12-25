a7bad5bcdab90cfc0ded19af585c2a37cd811276
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-24 21:48:14 -0500

    learn echo script

--- 01-sample.sh
+++ 01-sample.sh
@@ -4,3 +4,6 @@
 # You can have only one SheBang Notation per script and should always be the first file or start of the script 
 
 # Apart from the shebang notation, anything that starts with `#` is referred as a comment and is not executed by the script
+# Comments are just for hte human readable purpose only.
+
+echo "Hello World"
