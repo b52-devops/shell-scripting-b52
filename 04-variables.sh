8ab569a6dc463165955372d7d90ae19bb83eeeed
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-25 10:41:06 -0500

    Learn vriables

---
+++ 04-variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# What is variable ?
+# A variable is somehting which holds the value or data which would keep on changing as per the situation and scenario's
+# Typically we are telling the computer /program that uses this variable when running the scripts
+
+
+a=10
+b=20
+c=30
+
+# How to print the value of a variable 
+
+echo a
+
+# $a : telling system to print the value of a
+
+echo $a
