78cce53f52ff7ff0c45e61d7781f22f487e2b3f5
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-24 21:37:58 -0500

    Added sample.sh

---
+++ 01-sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# SheBang Notation tells the interpretor what the code and based on that it will understand and compile/run the script 
+# You can have only one SheBang Notation per script and should always be the first file or start of the script 
+
+# Apart from the shebang notation, anything that starts with `#` is referred as a comment and is not executed by the script
