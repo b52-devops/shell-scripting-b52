5fbb14289f89cbd6004a3fd1d122a41f71b890a5
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-24 22:56:58 -0500

    add lines in  echo script

--- 03-colours.sh
+++ 03-colours.sh
@@ -1,3 +1,11 @@
-!#/bin/bash
+#!/bin/bash 
 
-echo -e "\e[32m I am printing Green colour \e[0m"
+
+# Once you enable the color, ensure you close the color with \e[0m, if not color won't end
+
+# Prining Foreground text
+echo -e "\e[32m I am printing Green Color \e[0m"
+echo -e "\e[31m I am printing Red Color \e[0m"
+echo -e "\e[33m I am printing Yellow Color \e[0m"
+echo -e "\e[34m I am printing Blue Color \e[0m"
+echo -e "\e[35m I am printing Cyan Color \e[0m"
