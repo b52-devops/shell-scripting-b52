433d77cb786e167fb8225829f6e5a208d8379fab
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 15:36:58 -0500

    Learn functions

--- 08-function.sh
+++ 08-function.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-Declaring a sample function
-sample(){
-echo "I am a sample function"
-echo "IF you want to call me, just type sample"
-echo "sample function is completed"
-}
+# Declaring a sample function
+# sample(){
+# echo "I am a sample function"
+# echo "IF you want to call me, just type sample"
+# echo "sample function is completed"
+# }
 
 # Calling the sample function
 sample
