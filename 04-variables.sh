8eaacfca9e8281eec65cef9dd72c2b4f503d8a0a
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 13:07:22 -0500

    Update valriables

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,28 +4,30 @@
 # A variable is somehting which holds the value or data which would keep on changing as per the situation and scenario's
 # Typically we are telling the computer /program that uses this variable when running the scripts
 
-
-a=10
+a=10 
 b=20
-c=30
+c=30 
 d=100
+# JUNK_FOLDER_NAME=xyz
 
 # How to print the value of a variable 
-
-echo a
+echo a 
 
 # $a : telling system to print the value of a
 # $  : is a specia variable , when added as a prefix to any variable it considers to extract the value from the variable
 
-echo Value of a is $a
-echo value of a is ${a}
-
-# $a or ${a} both of them are same
+echo value of a is $a 
+echo value of a is ${a} 
 
 echo "Printing value of c : $c" 
 echo "Printing the value of d : $d"
 
+d=200
+echo "Printing the value of d : $d"
+
+# $a or ${a} both of them are same
+
 # If you don't declare a value and if you try to print it, it's going to consider that value as null.
- 
+
 # echo "Deleting the junk directory : "
 # rm -rf /data/${JUNK_FOLDER_NAME}   --->  rm -rf /data/ 
