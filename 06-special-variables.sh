807ba6d8e939a5913b24d284b1a781e734eacc60
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 14:15:22 -0500

    Learn dynamic vars

---
+++ 06-special-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Special Variable are pre-defined, which means you cannot create them, you can only use them.
+# Here are few of the special variables.
+
+###   $0 to $n , $*  $& $#  $? 
+
+
+# $0       : Prints the script name 
+# $?       : Shows the exit code of the previos command  
+# $1 to $9 : command line variables ; ex : sh test.sh  100 200 300 
+# $* or $@ : Prints all the variables used in this script
+# $#       : Prints the number variables used in this script
+
+echo "Name of script is $0"
