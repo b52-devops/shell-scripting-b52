e46e85750b5a138b14d8b56f70aec692f362bab9
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 13:47:55 -0500

    Learn dynamic vars

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,6 +2,7 @@
 
 DATE=$(date +%x)
 echo "Good morning. todays date is $DATE"
+echo "Total number of users signed in is $(who | wc -l"
 
 # Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
 
