3e7804744d2f77cb485b713570e59f1bfe73bb53
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 13:16:38 -0500

    Learn dynamic vars

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-DATE=$(date)
+DATE=$(date +%x)
 echo "Good morning. todays date is $DATE"
 
 # Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
+
+# () : Paranthesis      : Expressions should be enclused in paranthesis 
+# {} : Flower Brackets
+# [] : Square Brackets 
