7115ed715e9fa51c225a10655d74cea9390f2657
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-26 13:55:51 -0500

    Learn dynamic vars

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +%x)
-echo -e "Good morning. todays date is \e[31m $DATE \e[0m"
+echo -e "Good morning. todays date is \e[37m $DATE \e[0m"
 echo -e "Total number of opened sessions is \e[31m $(who | wc -l) \e[0m"
 
 # Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
