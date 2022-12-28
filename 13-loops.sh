012f14d4ba5ec82710a5a36ba28958953f8b5065
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 10:08:34 -0500

    Learn loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -4,6 +4,15 @@
 #     echo "Value is $list"
 # done
 
+# For loop will be used  when the loop is based on inputs
+
 for courses in devops aws gcp azure terraform ansible docker ; do
     echo "Course name is $courses"
+done
+
+# while loop : a conditional loop   
+
+i=10
+while [ $i -gt 0 ] ; do
+    echo "Iteration Number is $i"
 done
