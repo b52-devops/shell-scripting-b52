5378360a0e9fc0f6f8229c7d2004bbbef5d2e96f
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 09:48:12 -0500

    Learn if and elif

--- 12-if.sh
+++ 12-if.sh
@@ -16,3 +16,23 @@ else
     echo -e "\e[31m Both of them are NOT equal \e[0m"
     exit 1
 fi
+
+# Note :  Use ``==`` when you're dealing with strings, use -eq when dealing with numbers.
+
+# Demo on else if 
+
+c=$1
+
+if [ "$c" -eq "10" ] ; then
+    echo "Value of c is 10"
+
+elif [ "$c" -eq "20" ] ; then
+    echo "Value of c is 20"
+
+elif [ "$c" -eq "30" ] ; then
+    echo "Value of c is 30"
+
+else
+    echo "Value of c is NOT 10 or 20 or 30"
+
+fi
