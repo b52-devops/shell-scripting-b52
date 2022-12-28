cf293a568d733e637bc3b7b7711f16e3db6f3a90
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 18:59:47 -0500

    Learn automate roboshop

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -7,14 +7,14 @@ if [ "$ID" -ne 0 ] ; then
     exit 1 
 fi
 
-echo "Installing Nginx :"
+echo -n "Installing Nginx :"
 yum install nginx -y        &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m"
 else
     echo -e "\e[31m Failure \e[0m"
 fi
-echo "Starting Nginx :"
+echo -n "Starting Nginx :"
 
 systemctl enable nginx      &>> /tmp/frontend.log
 systemctl start nginx       &>> /tmp/frontend.log
