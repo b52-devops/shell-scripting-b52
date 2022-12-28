f4b7f3ebe76dcad745b0fd0bab18fe5a6ac047aa
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 10:05:24 -0500

    Learn loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-for list in val1 val2 val3 val4 val5 ; do 
-    echo "Value is $list"
+# for list in val1 val2 val3 val4 val5 ; do 
+#     echo "Value is $list"
+# done
+
+for courses in devops aws gcp azure = ansible terraform docker ; do
+    echo "Course name is $courses"
 done
