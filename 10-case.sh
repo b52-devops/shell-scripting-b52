602b5d06cb5a841583b8dba0b23a5778d0488701
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 09:30:58 -0500

    Learn case

--- 10-case.sh
+++ 10-case.sh
@@ -23,3 +23,5 @@ case $ACTION in
     echo -e  "\e[31m Valid options are start or stop or restart only \e[0m"
     exit 1
 esac
+
+# Range of the exit code is 0 to 255, whilst 0 is success code and rest of them are either partially failure or failure, or partially success
