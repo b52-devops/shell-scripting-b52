15ea5baddf8955924c4925c2cbff83a6a7ed3c6f
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 09:29:04 -0500

    Learn case

--- 10-case.sh
+++ 10-case.sh
@@ -9,12 +9,15 @@ ACTION=$1
 case $ACTION in
     start)
         echo "XYZ Service is starting"
+        exit 0
         ;;
     stop)
         echo "XYZ Service is stopping"
+        exit 0
         ;;
     restart)
         echo "XYZ Service is restarting"
+        exit 0
         ;;
     *)
     echo -e  "\e[31m Valid options are start or stop or restart only \e[0m"
