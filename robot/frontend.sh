57e055f55b82c1d9e0134e0216e4497340e5d403
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 10:35:10 -0500

    Automate roboshop project

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -3,7 +3,7 @@
 UID=$(id -u)
 
 if [ "$UID" -ne 0 ] ; then
-    echo  -e "\e[32m You need to run the script as a root user or with a sudo prevililege \e[0m"
+    echo  -e "\e[31m You need to run the script as a root user or with a sudo prevililege \e[0m"
     exit 1 
 fi
 yum install nginx -y
