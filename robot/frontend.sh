62330c4bdb1287ff02c7d6bfe06d47ffaace429d
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 18:42:28 -0500

    Learn automate roboshop

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-UID=$(id -u)
+ID=$(id -u)
 
-if [ "$UID" -ne 0 ] ; then
+if [ "$ID" -ne 0 ] ; then
     echo  -e "\e[31m You need to run the script as a root user or with a sudo prevililege \e[0m"
     exit 1 
 fi
+
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
