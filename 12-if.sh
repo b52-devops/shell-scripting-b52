aee6d90b30cfee51816c4356789f90a9717f6a28
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 09:49:28 -0500

    Learn if and elif

--- 12-if.sh
+++ 12-if.sh
@@ -9,13 +9,13 @@ a="ABC"
 
 # If else condition
 
-if [ "$a" == "xyz" ]; then
-    echo -e "\e[32m Both of them are equal \e[0m"
-    exit 0
-else
-    echo -e "\e[31m Both of them are NOT equal \e[0m"
-    exit 1
-fi
+# if [ "$a" == "xyz" ]; then
+#     echo -e "\e[32m Both of them are equal \e[0m"
+#     exit 0
+# else
+#     echo -e "\e[31m Both of them are NOT equal \e[0m"
+#     exit 1
+# fi
 
 # Note :  Use ``==`` when you're dealing with strings, use -eq when dealing with numbers.
 
