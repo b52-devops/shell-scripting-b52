21e2b54176980c38ab2b1b85388c1600ecbcf4c7
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 10:34:17 -0500

    Automate roboshop project

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+UID=$(id -u)
+
+if [ "$UID" -ne 0 ] ; then
+    echo  -e "\e[32m You need to run the script as a root user or with a sudo prevililege \e[0m"
+    exit 1 
+fi
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
