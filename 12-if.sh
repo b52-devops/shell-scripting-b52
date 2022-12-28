140fead5b77ce8c683654dc0f23202387caec46d
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 09:41:44 -0500

    Learn comments

--- 12-if.sh
+++ 12-if.sh
@@ -2,7 +2,16 @@
 
 a="xyz"
 
-if [ "$a" == "xyz" ]; then
+# if [ "$a" == "xyz" ]; then
+#     echo -e "\e[32m Both of them are equal \e[0m"
+
+# fi
+
+# If else condition
+
+if [ "$a" == "xyz" ]; than
     echo -e "\e[32m Both of them are equal \e[0m"
+else
+    echo -e "\e[31m Both of them are NOT equal \e[0m"
 
 fi
