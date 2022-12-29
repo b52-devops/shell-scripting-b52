ba941af67dfa97ab21075e30acf40ad480091787
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-29 16:19:02 -0500

    Automate roboshop

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -19,7 +19,8 @@ echo -n "Downloading the $COMPONENT : "
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
 stat $?
 
-echo -n "Extracting the $COMPONENT : "
+echo -n "Cleanup and Extracting the $COMPONENT : "
+rm -rf /home/$APPUSER/$COMPONENT/
 cd /home/$APPUSER
 unzip -o /tmp/$COMPONENT.zip     &>> ${LOGFILE}
 stat $?
