a96b0f8c9f8b57692d524e71d241f807c8584916
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-29 16:03:12 -0500

    Automate roboshop

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -25,7 +25,7 @@ unzip -o /tmp/$COMPONENT.zip     &>> ${LOGFILE}
 stat $?
 
 echo -n "Changing the ownership to $APPUSER : "
-mv /home/$APPUSER/$COMPONENT-main  /home/$APPUSER/$COMPONENT
+mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
 chown $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT
 stat $?
 
