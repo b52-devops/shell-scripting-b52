1e8a3cfd16e776fadae32fbc7f2a9fdea456013d
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-29 17:22:28 -0500

    Automate roboshop

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -5,33 +5,33 @@ COMPONENT=mongodb
 source components/common.sh    # source loads a file and this file has all the common patterns
 
 echo -n "Downloading $COMPONENT : "
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
 stat $?
 
 echo -n "Installing $COMPONENT : "
-yum install -y mongodb-org  &>> $LOGFILE
+yum install -y $COMPONENT-org  &>> $LOGFILE
 stat $?
 
 echo -n "Whitelisting the $COMPONENT"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$COMPONENT.conf
 stat $?
 
 echo -n "Starting $COMPONENT : "
-systemctl enable mongod
-systemctl start mongod
+systemctl enable $COMPONENT
+systemctl start $COMPONENT
 stat $?
 
 echo -n "Downloading the $COMPONENT schema : "
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 echo -n "Extracting the $COMPONENT schema file : "
 cd /tmp
-unzip -o mongodb.zip       &>> $LOGFILE
+unzip -o $COMPONENT.zip       &>> $LOGFILE
 stat $?
 
 echo -n "Injecting the schema : "
-cd mongodb-main
+cd $COMPONENT-main
 mongo < catalogue.js     &>> $LOGFILE
 mongo < users.js         &>> $LOGFILE
 stat $?
