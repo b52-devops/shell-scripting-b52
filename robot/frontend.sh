47bedcaa3ae9fbd3d4a3c79e84851e90581759a8
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 19:12:16 -0500

    Learn automate roboshop

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 COMPONENT=frontend
 
 ID=$(id -u)
@@ -30,7 +32,7 @@ cd /usr/share/nginx/html
 rm -rf *    &>> /tmp/frontend.log
 stat $?
 echo -n "Extracting $COMPONENT : "
-unzip /tmp/$COMPONENT.zip    &>> /tmp/frontend.log
+unzip /tmp/COMPONENT.zip    &>> /tmp/frontend.log
 stat $?
 
 echo -n "Copying $COMPONENT : "
