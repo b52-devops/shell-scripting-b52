15d0540f3114eb495f8eec9ab0a7f26645783286
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-29 11:46:09 -0500

    Learn automate roboshop

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -3,7 +3,7 @@ set -e
 
 COMPONENT=catalogue
 
-source robot/common.sh    # source loads a file and this file has all the common patterns
+source common.sh    # source loads a file and this file has all the common patterns
 
 echo -n "Configuring NodeJS repo : "
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -    &>> $LOGFILE
