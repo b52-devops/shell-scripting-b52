325d1a69676e50f94f20f3d471f98d4520994859
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-12-28 19:10:57 -0500

    Learn automate roboshop

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -20,21 +20,27 @@ echo -n "Installing Nginx :"
 yum install nginx -y        &>> /tmp/frontend.log
 stat $?
 
-echo -n "Starting Nginx :"
-systemctl enable nginx      &>> /tmp/frontend.log
-systemctl start nginx       &>> /tmp/frontend.log
-stat $?
-
-echo -n "Downloading the $COMPONENT "
+echo -n "Downloading the $COMPONENT :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 
 stat $?
 
+echo -n "Clearing the old default content: "
+cd /usr/share/nginx/html
+rm -rf *    &>> /tmp/frontend.log
+stat $?
+echo -n "Extracting $COMPONENT : "
+unzip /tmp/COMPONENT.zip    &>> /tmp/frontend.log
+stat $?
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Copying $COMPONENT : "
+mv frontend-main/* .     &>> /tmp/frontend.log
+mv static/* .            &>> /tmp/frontend.log
+rm -rf frontend-main README.md       &>> /tmp/frontend.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf         &>> /tmp/frontend.log
+stat $?
+
+echo -n "Restarting Nginx :"
+systemctl enable nginx      &>> /tmp/frontend.log
+systemctl restart nginx       &>> /tmp/frontend.log
+stat $?
