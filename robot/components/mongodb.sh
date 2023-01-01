ca4fae9f4cc837c072ebfabd72eace72427c6197
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-01-01 10:11:38 -0500

    Automate roboshop

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,39 +1,37 @@
-#!/bin/bash
+#!/bin/bash 
 
 COMPONENT=mongodb
 
-source components/common.sh    # source loads a file and this file has all the common patterns
+source components/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Downloading $COMPONENT : "
-curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo
-stat $?
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $? 
 
 echo -n "Installing $COMPONENT : "
-yum install -y $COMPONENT-org  &>> $LOGFILE
+yum install -y mongodb-org   &>> "${LOGFILE}"
 stat $?
 
-echo -n "Whitelisting the $COMPONENT"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo -n "whitelisting the mongodb"
+sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
 echo -n "Starting $COMPONENT : "
 systemctl enable mongod
 systemctl start mongod
-stat $?
+stat $? 
 
-echo -n "Downloading the $COMPONENT schema : "
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
+echo -n "Downloading the $COMPONENT Schema : "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+stat $? 
 
-echo -n "Extracting the $COMPONENT schema file : "
+echo -n "Extracting the $COMPONENT Schema file:"
 cd /tmp
-unzip -o $COMPONENT.zip       &>> $LOGFILE
-stat $?
+unzip -o mongodb.zip   &>> "${LOGFILE}"
+stat $? 
 
-echo -n "Injecting the schema : "
-cd $COMPONENT-main
-mongo < catalogue.js     &>> $LOGFILE
-mongo < users.js         &>> $LOGFILE
-stat $?
-
-echo -e "\e[32m ________ $COMPONENT Configuration Completed _________ \e[0m"
+echo -n "Injecting the Schema : "
+cd mongodb-main
+mongo < catalogue.js  &>> "${LOGFILE}"
+mongo < users.js      &>> "${LOGFILE}"
+stat $? 
