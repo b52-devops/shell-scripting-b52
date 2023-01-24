ac2e675fae0fd47cb064f168d0e0059119099ec4
jaindeepak2008 <jaindeepak2008@gmail.com>
2023-01-23 20:52:17 -0500

    autopush

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -10,7 +10,8 @@ if [ -z "$COMPONENT" ] || [ -z "$ENV" ]; then
     exit 1
 fi
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
+# AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
+AMI_ID="b52-ansible-dev-23jan23"
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
 echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m"
